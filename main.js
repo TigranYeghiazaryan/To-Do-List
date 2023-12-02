b516360991bd7dea942b11b28424180e0bb88d66
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-12-02 19:08:38 +0400

    FEATURES

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const form = document.querySelector("[data-form]");
+const lists = document.querySelector("[data-lists");
+const input = document.querySelector("[data-input]");
+
+let todoArr = [];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let id = Math.random() + 1000000;
+    const todo = new ToDo(id, input.value)
+    todoArr = [...todoArr, todo];
+    console.log(todoArr)
+});
+
+class ToDo {
+    constructor(id, todo){
+        this.id = id;
+        this.todo = todo;
+    }
+}
